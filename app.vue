1ee6979ae1f2252da9f09649d9d5da797ea65844
Erik Thalén <erik.thalen@hyperisland.se>
2023-10-17 11:05:20 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <nuxt-page />
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<script setup>
+const url = 'api.sampleapis.com/coffee/hot'
+
+const { data } = await useFetch('/api/cms', {
+  query: { url },
+})
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li><nuxt-link to="/">Index</nuxt-link></li>
+      <li><nuxt-link to="/about">About</nuxt-link></li>
+    </ul>
+    <code>
+      <pre>{{ data }}</pre>
+    </code>
+  </div>
+</template>
